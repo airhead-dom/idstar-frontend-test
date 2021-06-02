debb66d1842d37d0a36bb7ef67642d8311caf4be
Dom <flying.deiva@gmail.com>
2021-06-03 02:45:08 +0700

    done without test

--- src/features/contact/components/ContactItem.jsx
+++ src/features/contact/components/ContactItem.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Index from "../../../components/Image";
+import style from "../Contact.module.css";
 
-const ContactItem = ({ contact }) => {
+const imageSize = '60px';
+const actionSize = '30px';
+
+const ContactItem = ({contact, onClick = undefined, onDeleteClick = undefined}) => {
+  const {id, firstName, lastName, age, photo} = contact;
+  
+  const _onContactItemClick = () => {
+    if (onClick)
+      onClick(id);
+  };
+  
+  const _onDeleteClick = (e) => {
+    e.stopPropagation();
+    onDeleteClick(id);
+  }
+  
   return (
-      <div>
-        <p>{contact.firstName}</p>
-        <p>test</p>
+      <div className={style['contact-item']} onClick={_onContactItemClick}>
+        <Index src={photo} alt={`${firstName} ${lastName}`} width={imageSize} height={imageSize} circular={true}/>
+        <div className={style['info']} style={{width: `calc(100% - ${imageSize} - ${actionSize})`}}>
+          <p className={style.name}>{`${firstName} ${lastName}`}</p>
+          <p className={style.age}>{age} years old</p>
+        </div>
+        <div className={style.action} style={{width: actionSize}}>
+          <button className={style.delete} type='button' onClick={_onDeleteClick}>&times;</button>
+        </div>
       </div>
   );
 };
@@ -17,7 +40,9 @@ ContactItem.propTypes = {
     lastName: PropTypes.string,
     age: PropTypes.number,
     photo: PropTypes.string
-  })
+  }),
+  onClick: PropTypes.func,
+  onDeleteClick: PropTypes.func
 };
 
 export default ContactItem;
